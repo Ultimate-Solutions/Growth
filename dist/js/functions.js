55f369391feeb59b9a55befcb4b68c58d53af990
Majed Sief Alnasr <majedsiefalnasr@invsky.com>
2022-02-01 14:32:21 +0200

    Client side theme switcher

---
+++ dist/js/functions.js
@@ -0,0 +1,10 @@
+// console.log(document.cookie);
+// document.cookie = 'theme=dark;';
+// document.cookie = 'themed=dark;';
+// document.cookie = 'themef=dark;';
+// console.log(document.cookie);
+// localStorage.setItem('theme', 'dark');
+// document
+// 	.querySelector('html')
+// 	.setAttribute('data-color-mode', localStorage.getItem('theme'));
+// console.log(localStorage.getItem('theme'));
