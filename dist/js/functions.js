ac3e1872cef2f5cf3ded142d7a3a8f25b5fd2dc4
Majed Sief Alnasr <majedsiefalnasr@invsky.com>
2022-02-21 12:54:11 +0200

    Add Apexchart for home

--- dist/js/functions.js
+++ dist/js/functions.js
@@ -510,10 +510,528 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 // #endregion
 
+// #region / Apexcharts
+document.addEventListener('DOMContentLoaded', () => {
+  // Small Profits Chart
+  var ele1 = document.getElementById('holol_chart_widget_1_small');
+  // Check exist
+  if (ele1) {
+    // Options
+    var options = {
+      series: [
+        {
+          name: 'Net Profit',
+          data: [10, 10, 34, 16, 10],
+        },
+      ],
+      chart: {
+        fontFamily: 'inherit',
+        type: 'area',
+        height: '30px',
+        toolbar: {
+          show: !1,
+        },
+        zoom: {
+          enabled: !1,
+        },
+        sparkline: {
+          enabled: !0,
+        },
+      },
+      plotOptions: {},
+      legend: {
+        show: !1,
+      },
+      dataLabels: {
+        enabled: !1,
+      },
+      fill: {
+        type: 'solid',
+        opacity: 1,
+      },
+      stroke: {
+        curve: 'straight',
+        show: !0,
+        width: 3,
+        colors: [getCssVariableValue('--bs-success')],
+      },
+      xaxis: {
+        categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov'],
+        axisBorder: {
+          show: !1,
+        },
+        axisTicks: {
+          show: !1,
+        },
+      },
+      yaxis: {
+        labels: {
+          show: !1,
+        },
+      },
+      states: {
+        normal: {
+          filter: {
+            type: 'none',
+            value: 0,
+          },
+        },
+        hover: {
+          filter: {
+            type: 'none',
+            value: 0,
+          },
+        },
+        active: {
+          allowMultipleDataPointsSelection: !1,
+          filter: {
+            type: 'none',
+            value: 0,
+          },
+        },
+      },
+      tooltip: {
+        show: !1,
+      },
+      colors: ['transparent'],
+    };
+    // Call function
+    new ApexCharts(ele1, options).render();
+  }
+
+  // Small Sales Chart
+  var ele2 = document.getElementById('holol_chart_widget_2_small');
+  // Check exist
+  if (ele2) {
+    // Options
+    var options = {
+      series: [
+        {
+          name: 'Net Profit',
+          data: [10, 8, 18, 16, 24],
+        },
+      ],
+      chart: {
+        fontFamily: 'inherit',
+        type: 'area',
+        height: '30px',
+        toolbar: {
+          show: !1,
+        },
+        zoom: {
+          enabled: !1,
+        },
+        sparkline: {
+          enabled: !0,
+        },
+      },
+      plotOptions: {},
+      legend: {
+        show: !1,
+      },
+      dataLabels: {
+        enabled: !1,
+      },
+      fill: {
+        type: 'solid',
+        opacity: 1,
+      },
+      stroke: {
+        curve: 'straight',
+        show: !0,
+        width: 3,
+        colors: [getCssVariableValue('--bs-indigo')],
+      },
+      xaxis: {
+        categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov'],
+        axisBorder: {
+          show: !1,
+        },
+        axisTicks: {
+          show: !1,
+        },
+      },
+      yaxis: {
+        labels: {
+          show: !1,
+        },
+      },
+      states: {
+        normal: {
+          filter: {
+            type: 'none',
+            value: 0,
+          },
+        },
+        hover: {
+          filter: {
+            type: 'none',
+            value: 0,
+          },
+        },
+        active: {
+          allowMultipleDataPointsSelection: !1,
+          filter: {
+            type: 'none',
+            value: 0,
+          },
+        },
+      },
+      tooltip: {
+        show: !1,
+      },
+      colors: ['transparent'],
+    };
+    // Call function
+    new ApexCharts(ele2, options).render();
+  }
+
+  // Small Orders Chart
+  var ele3 = document.getElementById('holol_chart_widget_3_small');
+  // Check exist
+  if (ele3) {
+    // Options
+    var options = {
+      series: [
+        {
+          name: 'Net Profit',
+          data: [10, 20, 20, 16, 10],
+        },
+      ],
+      chart: {
+        fontFamily: 'inherit',
+        type: 'area',
+        height: '30px',
+        toolbar: {
+          show: !1,
+        },
+        zoom: {
+          enabled: !1,
+        },
+        sparkline: {
+          enabled: !0,
+        },
+      },
+      plotOptions: {},
+      legend: {
+        show: !1,
+      },
+      dataLabels: {
+        enabled: !1,
+      },
+      fill: {
+        type: 'solid',
+        opacity: 1,
+      },
+      stroke: {
+        curve: 'straight',
+        show: !0,
+        width: 3,
+        colors: [getCssVariableValue('--bs-warning')],
+      },
+      xaxis: {
+        categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov'],
+        axisBorder: {
+          show: !1,
+        },
+        axisTicks: {
+          show: !1,
+        },
+      },
+      yaxis: {
+        labels: {
+          show: !1,
+        },
+      },
+      states: {
+        normal: {
+          filter: {
+            type: 'none',
+            value: 0,
+          },
+        },
+        hover: {
+          filter: {
+            type: 'none',
+            value: 0,
+          },
+        },
+        active: {
+          allowMultipleDataPointsSelection: !1,
+          filter: {
+            type: 'none',
+            value: 0,
+          },
+        },
+      },
+      tooltip: {
+        show: !1,
+      },
+      colors: ['transparent'],
+    };
+    // Call function
+    new ApexCharts(ele3, options).render();
+  }
+
+  // Small views Chart
+  var ele4 = document.getElementById('holol_chart_widget_4_small');
+  // Check exist
+  if (ele4) {
+    // Options
+    var options = {
+      series: [
+        {
+          name: 'Net Profit',
+          data: [8, 20, 18, 20, 32],
+        },
+      ],
+      chart: {
+        fontFamily: 'inherit',
+        type: 'area',
+        height: '30px',
+        toolbar: {
+          show: !1,
+        },
+        zoom: {
+          enabled: !1,
+        },
+        sparkline: {
+          enabled: !0,
+        },
+      },
+      plotOptions: {},
+      legend: {
+        show: !1,
+      },
+      dataLabels: {
+        enabled: !1,
+      },
+      fill: {
+        type: 'solid',
+        opacity: 1,
+      },
+      stroke: {
+        curve: 'straight',
+        show: !0,
+        width: 3,
+        colors: [getCssVariableValue('--bs-green')],
+      },
+      xaxis: {
+        categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov'],
+        axisBorder: {
+          show: !1,
+        },
+        axisTicks: {
+          show: !1,
+        },
+      },
+      yaxis: {
+        labels: {
+          show: !1,
+        },
+      },
+      states: {
+        normal: {
+          filter: {
+            type: 'none',
+            value: 0,
+          },
+        },
+        hover: {
+          filter: {
+            type: 'none',
+            value: 0,
+          },
+        },
+        active: {
+          allowMultipleDataPointsSelection: !1,
+          filter: {
+            type: 'none',
+            value: 0,
+          },
+        },
+      },
+      tooltip: {
+        show: !1,
+      },
+      colors: ['transparent'],
+    };
+    // Call function
+    new ApexCharts(ele4, options).render();
+  }
+
+  // Big home Chart
+  var eleHome = document.getElementById('holol_chart_widget_1');
+  // Check exist
+  if (eleHome) {
+    // Options
+    var options = {
+      series: [
+        {
+          name: 'الارباح',
+          data: [30000, 40000, 40000, 90000, 90000, 70000, 70000, 30000],
+        },
+        {
+          name: 'المبيعات',
+          data: [40000, 40000, 60000, 80000, 60000, 60000, 60000, 50000],
+        },
+        {
+          name: 'الطلبات',
+          data: [20000, 30000, 50000, 20000, 20000, 60000, 80000, 10000],
+        },
+        {
+          name: 'الزيارات',
+          data: [0000, 10000, 40000, 40000, 40000, 80000, 80000, 50000],
+        },
+      ],
+      chart: {
+        fontFamily: 'inherit',
+        type: 'area',
+        height: 350,
+        toolbar: {
+          show: !1,
+        },
+      },
+      plotOptions: {},
+      legend: {
+        show: !1,
+      },
+      dataLabels: {
+        enabled: !1,
+      },
+      fill: {
+        type: 'solid',
+        opacity: 1,
+      },
+      stroke: {
+        curve: 'smooth',
+        show: !0,
+        width: 3,
+        colors: [
+          getCssVariableValue('--chart-blue'),
+          getCssVariableValue('--chart-pink'),
+          getCssVariableValue('--chart-yellow'),
+          getCssVariableValue('--chart-green'),
+        ],
+      },
+      xaxis: {
+        categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+        axisBorder: {
+          show: !1,
+        },
+        axisTicks: {
+          show: !1,
+        },
+        labels: {
+          style: {
+            colors: [getCssVariableValue('--bs-gray')],
+            fontSize: '12px',
+          },
+        },
+        crosshairs: {
+          position: 'front',
+          stroke: {
+            color: getCssVariableValue('--bs-primary'),
+            width: 1,
+            dashArray: 3,
+          },
+        },
+        tooltip: {
+          enabled: !0,
+          formatter: void 0,
+          offsetY: 0,
+          style: {
+            fontSize: '12px',
+          },
+        },
+      },
+      yaxis: {
+        labels: {
+          style: {
+            colors: [getCssVariableValue('--bs-gray')],
+            fontSize: '12px',
+          },
+        },
+      },
+      states: {
+        normal: {
+          filter: {
+            type: 'none',
+            value: 0,
+          },
+        },
+        hover: {
+          filter: {
+            type: 'none',
+            value: 0,
+          },
+        },
+        active: {
+          allowMultipleDataPointsSelection: !1,
+          filter: {
+            type: 'none',
+            value: 0,
+          },
+        },
+      },
+      tooltip: {
+        style: {
+          fontSize: '12px',
+        },
+        y: {
+          formatter: function (t) {
+            if (t < 1000) return t;
+            else if (t >= 1000) return t / 1000 + 'K';
+            else if (t >= 1000000) return t / 1000000 + 'M';
+            else if (t >= 1000000000) return t / 1000000000 + 'T';
+          },
+        },
+      },
+      colors: [
+        getCssVariableValue('--chart-blue-opacity'),
+        getCssVariableValue('--chart-pink-opacity'),
+        getCssVariableValue('--chart-yellow-opacity'),
+        getCssVariableValue('--chart-green-opacity'),
+      ],
+      grid: {
+        borderColor: getCssVariableValue('--chart-gray-border'),
+        strokeDashArray: 4,
+        yaxis: {
+          lines: {
+            show: !0,
+          },
+        },
+      },
+      markers: {
+        strokeColor: [
+          getCssVariableValue('--chart-blue'),
+          getCssVariableValue('--chart-pink'),
+          getCssVariableValue('--chart-yellow'),
+          getCssVariableValue('--chart-green'),
+        ],
+        strokeWidth: 3,
+      },
+    };
+    // Call function
+    new ApexCharts(eleHome, options).render();
+  }
+});
+// #endregion
+
 // Element Top Left position helper
 function offset(el) {
   var rect = el.getBoundingClientRect(),
     scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
     scrollTop = window.pageYOffset || document.documentElement.scrollTop;
   return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
+}
+
+// Get CSS Variable Value helper
+function getCssVariableValue(variableName) {
+  var hex = getComputedStyle(document.documentElement).getPropertyValue(variableName);
+  if (hex && hex.length > 0) {
+    hex = hex.trim();
+  }
+
+  return hex;
 }
