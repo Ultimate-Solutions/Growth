d26702c2becda46375c18382022a57d780b5d65c
Majed Sief Alnasr <majedsiefalnasr@invsky.com>
2022-02-27 16:06:16 +0200

    Add widgets js

---
+++ dist/js/widgets.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Class definition
+var HOLOLWidgets = (function () {
+  // Public methods
+  return {
+    init: function () {},
+  };
+})();
+
+// On document ready
+HOLOLUtil.onDOMContentLoaded(function () {
+  HOLOLWidgets.init();
+});
