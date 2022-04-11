3470e18abe16b2142f32576a7e4c21fead20921a
Majed Sief Alnasr <majedsiefalnasr@invsky.com>
2022-04-11 14:54:16 +0200

    Fix Bootstrap Multi-level dropdown

--- dist/js/app.bundle.js
+++ dist/js/app.bundle.js
@@ -1456,6 +1456,7 @@ var HOLOLApp = (function () {
       );
     });
 
+    console.log(promises);
     return promises;
   };
 
@@ -2543,6 +2544,57 @@ var HOLOLApp = (function () {
     $('table[shrink]').tableShrinker(options);
   };
 
+  var initMultiLevelBootstrapDropdown = function () {
+    // Bootstrap 5 Multiple Level Dropdown
+    // MIT License
+    // https://github.com/dallaslu/bootstrap-5-multi-level-dropdown
+    (function ($bs) {
+      const CLASS_NAME = 'has-child-dropdown-show';
+      $bs.Dropdown.prototype.toggle = (function (_orginal) {
+        return function () {
+          document.querySelectorAll('.' + CLASS_NAME).forEach(function (e) {
+            e.classList.remove(CLASS_NAME);
+          });
+          let dd = this._element.closest('.dropdown').parentNode.closest('.dropdown');
+          for (; dd && dd !== document; dd = dd.parentNode.closest('.dropdown')) {
+            dd.classList.add(CLASS_NAME);
+          }
+          return _orginal.call(this);
+        };
+      })($bs.Dropdown.prototype.toggle);
+
+      document.querySelectorAll('.dropdown').forEach(function (dd) {
+        dd.addEventListener('hide.bs.dropdown', function (e) {
+          if (this.classList.contains(CLASS_NAME)) {
+            this.classList.remove(CLASS_NAME);
+            e.preventDefault();
+          }
+          e.stopPropagation(); // do not need pop in multi level mode
+        });
+      });
+
+      // for hover
+      document
+        .querySelectorAll('.dropdown-hover, .dropdown-hover-all .dropdown')
+        .forEach(function (dd) {
+          dd.addEventListener('mouseenter', function (e) {
+            let toggle = e.target.querySelector(':scope>[data-bs-toggle="dropdown"]');
+            if (!toggle.classList.contains('show')) {
+              $bs.Dropdown.getOrCreateInstance(toggle).toggle();
+              dd.classList.add(CLASS_NAME);
+              $bs.Dropdown.clearMenus();
+            }
+          });
+          dd.addEventListener('mouseleave', function (e) {
+            let toggle = e.target.querySelector(':scope>[data-bs-toggle="dropdown"]');
+            if (toggle.classList.contains('show')) {
+              $bs.Dropdown.getOrCreateInstance(toggle).toggle();
+            }
+          });
+        });
+    })(bootstrap);
+  };
+
   // Public methods
   return {
     init: function () {
@@ -2573,6 +2625,7 @@ var HOLOLApp = (function () {
       this.initTableSelectorActions();
       this.initInputSwitch();
       this.initJQueryTableShrinker();
+      this.initMultiLevelBootstrapDropdown();
     },
 
     initHello: function (provider, version, link) {
@@ -2685,6 +2738,9 @@ var HOLOLApp = (function () {
     },
     initJQueryTableShrinker: function () {
       initJQueryTableShrinker();
+    },
+    initMultiLevelBootstrapDropdown: function () {
+      initMultiLevelBootstrapDropdown();
     },
   };
 })();
